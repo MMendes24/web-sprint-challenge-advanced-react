10cafa28914be18d76485ef367843e6288c010f3
MMendes24 <mmbusiness1@protonmail.com>
2020-08-07 10:52:02 -0400

    Plants rendering to page.

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -3,10 +3,39 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+ state = { 
+    plants: [
+      // {
+      // description: "",
+      // difficulty: "",
+      // id: "",
+      // img: "",
+      // light: "direct",
+      // name: "",
+      // price: "",
+      // scientificName: "",
+      // sizes: [],
+      // length: "",
+      // watering: "",
+      // }
+    ]
+  }
+  
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount() {
+    axios.get('http://localhost:3333/plants')
+    .then(res => {
+      this.setState({
+        plants : res.data.plantsData
+      })
+      console.log(res.data.plantsData)
+    })
+    .catch( () => {
+      console.log("Sorry New Orleans it happened again!")
+    })
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
