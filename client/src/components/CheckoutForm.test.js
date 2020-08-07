0c39d7a62f476606bf90a17f6967bd3641e67e3d
MMendes24 <mmbusiness1@protonmail.com>
2020-08-07 11:40:22 -0400

    Form entirely tested and tests are functional. Bad tests deliberately created to establish valid tests. MVP complete.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const formHeader = screen.getByRole('heading')
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />);
+
+    // lets store a lot of these in variables
+    const fnIn = screen.getByLabelText(/first name/i)
+    const lnIn = screen.getByLabelText(/last name/i)
+    const addrIn = screen.getByLabelText(/address/i)
+    const cityIn =screen.getByLabelText(/city/i)
+    const stateIn = screen.getByLabelText(/state/i)
+    const zip = screen.getByLabelText(/zip/i)
+    
+    //firing section: broadside! 
+    fireEvent.change(fnIn, { target: { value: 'Mars'} })
+    fireEvent.change(lnIn, { target: { value: 'Mendes'} })
+    fireEvent.change(addrIn, { target: { value: 'mars@mars.com'} })
+    fireEvent.change(cityIn, { target: { value: 'Biloxi'} })
+    fireEvent.change(stateIn, { target: { value: 'MS'} })
+    fireEvent.change(zip, { target: { value: '39530'} })
+
+    //let's define and click the much dreaded button
+    const checkoutB = screen.getByRole('button')
+    fireEvent.click(checkoutB)
+
+    expect(await screen.findByTestId('successMessage')).toBeInTheDocument()
+});
